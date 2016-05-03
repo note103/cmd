aa641950a290c79dca0e24bfb03e11723e81161a
Hiroaki Kadomatsu <note103@gmail.com>
2016-05-04 02:05:08 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+cmd - It's new $module
+
+# SYNOPSIS
+
+    use cmd;
+
+# DESCRIPTION
+
+cmd is ...
+
+# LICENSE
+
+Copyright (C) Hiroaki Kadomatsu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Hiroaki Kadomatsu <note103@gmail.com>
---
+++ lib/cmd.pm
@@ -0,0 +1,39 @@
+package cmd;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+cmd - It's new $module
+
+=head1 SYNOPSIS
+
+    use cmd;
+
+=head1 DESCRIPTION
+
+cmd is ...
+
+=head1 LICENSE
+
+Copyright (C) Hiroaki Kadomatsu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Hiroaki Kadomatsu E<lt>note103@gmail.comE<gt>
+
+=cut
+
